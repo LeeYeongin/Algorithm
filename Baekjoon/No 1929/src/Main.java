a9781041dacda4aed44a4db531fdd675c84a0377
LeeYeongin <youngin5757@gmail.com>
2025-08-26 09:35:26 +0900

    2025.08.25 백준 No.1929 소수 구하기

---
+++ Baekjoon/No 1929/src/Main.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        boolean[] isPrime = new boolean[N+1];
+        Arrays.fill(isPrime, true);
+
+        isPrime[0] = false;
+        isPrime[1] = false;
+
+        for(int i=2; i<=Math.sqrt(N); i++){
+            if(isPrime[i]){
+                for(int j=i*i; j<N+1; j += i){
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+
+        for(int i=M; i<N+1; i++){
+            if(isPrime[i]){
+                bw.write(i + "\n");
+            }
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
